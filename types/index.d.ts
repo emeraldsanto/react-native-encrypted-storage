0a0538cc6c7c2e4ee09d9c1168605d7c29402d64
Yanick <yanick.belanger@yahoo.com>
2020-02-15 16:01:12 -0500

    Changed api a little to be able to use callbacks, wrote tests and changed typedefs

--- types/index.d.ts
+++ types/index.d.ts
@@ -8,19 +8,19 @@ declare module "react-native-encrypted-storage" {
          * @param {string} key - A string that will be associated to the value for later retrieval.
          * @param {Object} value - The data to store.
          */
-        setItem(key : string, value : Object) : Promise<boolean>;
+        setItem(key : string, value : Object, callback? : (error? : Error) => void) : Promise<void>;
 
         /**
          * Retrieves data from the disk, using SharedPreferences or KeyChain, depending on the platform and returns it.
          * @param {string} key - A string that is associated to a value.
          */
-        getItem(key : string) : Promise<string | undefined>;
+        getItem(key : string, callback? : (error? : Error, value? : string) => void) : Promise<string | null>;
 
         /**
          * Deletes data from the disk, using SharedPreferences or KeyChain, depending on the platform.
          * @param {string} key - A string that is associated to a value.
          */
-        removeItem(key : string) : Promise<boolean>;
+        removeItem(key : string, callback? : (error? : Error) => void) : Promise<void>;
     }
 
     const EncryptedStorage : EncryptedStorageStatic;
