b8e61f20d1257d123a982909ecd0859c27f30db1
Yanick <yanick.belanger@yahoo.com>
2020-03-01 16:18:38 -0500

    SingletonList

--- android/src/main/java/com/emeraldsanto/encryptedstorage/RNEncryptedStoragePackage.java
+++ android/src/main/java/com/emeraldsanto/encryptedstorage/RNEncryptedStoragePackage.java
@@ -5,7 +5,6 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -13,10 +12,7 @@ public class RNEncryptedStoragePackage extends ReactPackage {
 
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-        List<NativeModule> modules = new ArrayList<>();
-        modules.add(new RNEncryptedStorageModule(reactContext));
-
-        return modules;
+        return Collections.<NativeModule>singletonList(new RNEncryptedStorageModule(reactContext));
     }
 
     @Override
