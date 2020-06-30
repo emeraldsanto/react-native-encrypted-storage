416ece911e34c1ef71908ae7a41b26d25df51042
emeraldsanto <yanick.belanger@yahoo.com>
2020-06-30 10:38:58 -0400

    Committing JS files

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var EncryptedStorage_1 = require("./EncryptedStorage");
+Object.defineProperty(exports, "default", { enumerable: true, get: function () { return EncryptedStorage_1.default; } });
