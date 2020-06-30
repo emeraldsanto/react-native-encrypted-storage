a36d26ebc17d3856dd016c086f8475308ed6e10e
emeraldsanto <yanick.belanger@yahoo.com>
2020-06-30 11:39:57 -0400

    Compiled js

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var EncryptedStorage_1 = require("./EncryptedStorage");
-exports.default = EncryptedStorage_1.default;
+Object.defineProperty(exports, "default", { enumerable: true, get: function () { return EncryptedStorage_1.default; } });
