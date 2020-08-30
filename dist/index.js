9a2bcc3fc2b129941f6b9ec170e5694a9f6a9adb
emeraldsanto <yanick.belanger@yahoo.com>
2020-08-30 17:03:27 -0400

    Untrack JS + version bump

--- dist/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var EncryptedStorage_1 = require("./EncryptedStorage");
-Object.defineProperty(exports, "default", { enumerable: true, get: function () { return EncryptedStorage_1.default; } });
