3d21c0f5f5865c310e16deb918a2a74025888d08
emeraldsanto <yanick.belanger@yahoo.com>
2022-06-12 10:18:50 -0400

    export as object in JS

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,18 @@
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
-import { getAllKeys, getItem, multiGet, multiRemove } from 'react-native-encrypted-storage';
+import EncryptedStorage from 'react-native-encrypted-storage';
 
 export default function App() {
   const [result, setResult] = React.useState<Array<string>>([]);
 
   React.useEffect(() => {
-    getAllKeys().then((keys) => {
+    EncryptedStorage.getAllKeys().then((keys) => {
       console.log('All keys', keys);
-      multiGet(keys).then((values) => {
+      EncryptedStorage.multiGet(keys).then((values) => {
         console.log(values);
-        multiRemove(keys).then(() => {
-          getAllKeys().then((remaining) => {
+        EncryptedStorage.multiRemove(keys).then(() => {
+          EncryptedStorage.getAllKeys().then((remaining) => {
             console.log('Remaining keys after deletion', remaining)
           })
         })
---
+++ src/encrypted-storage.ts
@@ -0,0 +1,119 @@
+import { NativeModules, Platform } from 'react-native';
+
+type StorageErrorCallback = (error: Error | null) => void;
+type StorageValueCallback<T> = (error: Error | null, value?: T) => void;
+
+const LINKING_ERROR =
+  `The package 'react-native-encrypted-storage' doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  '- You rebuilt the app after installing the package\n' +
+  '- You are not using Expo managed workflow\n';
+
+const fallback = new Proxy({}, {
+  get() {
+    throw new Error(LINKING_ERROR);
+  },
+});
+
+const module = NativeModules.EncryptedStorage
+  ? NativeModules.EncryptedStorage
+  : fallback;
+
+function getAllKeys(): Promise<Array<string>>;
+function getAllKeys(cb: StorageValueCallback<Array<string>>): void;
+function getAllKeys(cb?: StorageValueCallback<Array<string>>) {
+  if (cb) {
+    void module.getAllKeys()
+      .then((output: Array<string>) => cb(null, output))
+      .catch(cb);
+  } else {
+    return module.getAllKeys();
+  }
+}
+
+function getItem(key: string): Promise<string>;
+function getItem(key: string, cb: StorageValueCallback<string>): void;
+function getItem(key: string, cb?: StorageValueCallback<string>) {
+  const promise = module.multiGet([key]);
+
+  if (cb) {
+    void promise
+      .then((output: Array<string>) => cb(null, output[0]))
+      .catch(cb);
+  } else {
+    return promise
+      .then((output: Array<string>) => output[0]);
+  }
+}
+
+function setItem(key: string, value: string): Promise<void>;
+function setItem(key: string, value: string, cb: StorageErrorCallback): void;
+function setItem(key: string, value: string, cb?: StorageErrorCallback) {
+  const promise = module.multiSet([[key, value]]);
+
+  if (cb) {
+    void promise.then(cb).catch(cb);
+  } else {
+    return promise;
+  }
+}
+
+function removeItem(key: string): Promise<void>;
+function removeItem(key: string, cb: StorageErrorCallback): void;
+function removeItem(key: string, cb?: StorageErrorCallback) {
+  const promise = module.multiRemove([key]);
+
+  if (cb) {
+    void promise.then(cb).catch(cb);
+  } else {
+    return promise;
+  }
+}
+
+function multiGet(keys: Array<string>): Promise<Array<string>>;
+function multiGet(keys: Array<string>, cb: StorageValueCallback<Array<string>>): void;
+function multiGet(keys: Array<string>, cb?: StorageValueCallback<Array<string>>) {
+  const promise = module.multiGet(keys);
+
+  if (cb) {
+    void promise
+      .then((output: Array<string>) => cb(null, output))
+      .catch(cb);
+  } else {
+    return promise;
+  }
+}
+
+function multiSet(items: Array<[string, string]>): Promise<void>;
+function multiSet(items: Array<[string, string]>, cb: StorageErrorCallback): void;
+function multiSet(items: Array<[string, string]>, cb?: StorageErrorCallback) {
+  const promise = module.multiSet(items);
+
+  if (cb) {
+    void promise.then(cb).catch(cb);
+  } else {
+    return promise;
+  }
+}
+
+function multiRemove(keys: Array<string>): Promise<void>;
+function multiRemove(keys: Array<string>, cb: StorageErrorCallback): void;
+function multiRemove(keys: Array<string>, cb?: StorageErrorCallback) {
+  const promise = module.multiRemove(keys);
+
+  if (cb) {
+    void promise.then(cb).catch(cb);
+  } else {
+    return promise;
+  }
+}
+
+export const EncryptedStorage = {
+  getAllKeys,
+  getItem,
+  setItem,
+  removeItem,
+  multiGet,
+  multiSet,
+  multiRemove,
+}
